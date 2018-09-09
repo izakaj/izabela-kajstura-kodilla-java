3f797d4becab256339745f493ae935b9799f7183
izakaj <izakaj@gmail.com>
2018-09-09 16:04:07 +0200

    6.2 podejscie 2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,43 +6,22 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
 
-        // pierwszy test - Simple User
-
-        System.out.println("Test SimpleUser");
-
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK\n");
-        } else {
-            System.out.println("Error!\n");
-        }
-
-        // testy kalkulatora
-
         System.out.println("Test dodawania");
-
         Calculator calculator = new Calculator();
-
         int a = 5;
         int b = 7;
-
-        if(calculator.addAToB(a, b) == a + b) {
+        if (calculator.addAToB(a, b) == 13) {
             System.out.println("test OK\n");
         } else {
             System.out.println("Error!\n");
         }
 
         System.out.println("Test odejmowania");
-
-        if(calculator.subtractBFromA(a, b) == a - b) {
+        if (calculator.subtractBFromA(a, b) == -2) {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
         }
-
 
     }
 }
