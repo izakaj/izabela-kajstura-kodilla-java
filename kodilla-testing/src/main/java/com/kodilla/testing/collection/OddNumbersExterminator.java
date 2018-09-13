db05fc11e187e1c1ec68bf801b36232b536d609c
izakaj <izakaj@gmail.com>
2018-09-13 17:46:05 +0200

    Kodilla 6.3 ver 4

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -5,13 +5,9 @@ import java.util.List;
 
 public class OddNumbersExterminator {
 
-    private List<Integer> evenNumbers = new ArrayList<Integer>();
-
     public List<Integer> exterminate(List<Integer> numbers) {
 
-        if(evenNumbers.size() != 0){
-            evenNumbers.clear();
-        }
+        List<Integer> evenNumbers = new ArrayList<Integer>();
 
         if(numbers.size() == 0){
             System.out.println("The list is empty!");
